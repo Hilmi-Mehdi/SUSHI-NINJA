a01ba8ec4941f1d9aeacd4f28a69c6ea0536f0a6
Hilmi-Mehdi <76878548+Hilmi-Mehdi@users.noreply.github.com>
2021-01-03 12:55:59 +0100

    Update CrouchAbility.cs

--- CrouchAbility.cs
+++ CrouchAbility.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DiasGames.ThirdPersonSystem
+namespace SushiNinja.ThirdPersonSystem
 {
     public class CrouchAbility : ThirdPersonAbility
     {
